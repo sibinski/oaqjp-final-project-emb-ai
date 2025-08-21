91577e8ae2e46df26fdd6ea0eb3590cd55a2b0d1
sibinski <jevgeni.shibinski@gmail.com>
2025-08-21 06:07:28 -0400

    Added unit test, also improved main function's code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,12 +6,22 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = {"raw_document": { "text": text_to_analyse }}
     response = requests.post(url, json = myobj, headers=headers)
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotions, key=emotions.get)
-    result = emotions.copy()
-    result['dominant_emotion'] = dominant_emotion
-    return result
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
+    else:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotions, key=emotions.get)
+        result = emotions.copy()
+        result['dominant_emotion'] = dominant_emotion
+        return result
 
 if __name__ == "__main__":
     text = input("Please enter a phrase...\n")
